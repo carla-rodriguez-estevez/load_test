6f266835e712596da58ea1136235df7d9d92d94c
Carla Rodríguez Estévez <carla.rodriguez1@udc.es>
2023-03-04 23:26:51 +0100

    loaf test with k6 implemented

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule LoadTestApi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -41,7 +41,9 @@ defmodule LoadTestApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:k6, "~> 0.2.0"}
+
     ]
   end
 
---
+++ priv/k6/load_test.js
@@ -0,0 +1,27 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+// See https://k6.io/docs/using-k6/options/#using-options for documentation on k6 options
+export const options = {
+  vus: 10,
+  duration: '30s',
+  thresholds: { // See https://k6.io/docs/using-k6/thresholds/
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<200'],
+  }
+};
+
+export default function () {
+  // To set dynamic (e.g. environment-specific) configuration, pass it either as environment
+  // variable when invoking k6 or by setting `:k6, env: [key: "value"]` in your `config.exs`,
+  // and then access it from `__ENV`, e.g.: `const url = __ENV.url`
+
+  let baseUrl = "http://localhost:4000/api/animales/db2e47de-282b-42b8-bd79-9c93bdbf66e2";
+
+  // See https://k6.io/docs/using-k6/protocols/http-2/ for documentation on k6 and HTTP
+  let res = http.get(baseUrl);
+  check(res, { success: (r) => r.status === 200 });
+  sleep(0.3);
+
+
+}
